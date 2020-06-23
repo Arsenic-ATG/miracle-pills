4de698ff84e637fdc3549ae380212e56b3059bb0
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2020-06-23 11:52:58 +0530

    Initial Commit

---
+++ miracle pills/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  miracle pills
+//
+//  Created by Ankur Saini on 23/06/20.
+//  Copyright © 2020 Ankur Saini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
